c5531700ddef57368a7da6d71f274dc0a6f5c148
WaelAlturi <101098027+WaelAlturi@users.noreply.github.com>
2023-02-27 13:13:14 +0200

    270223

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from "express";
 import mongoose from "mongoose";
-
+import actions from './controllers/actions.js'
 const  app  = express();
 
 app.use(express.urlencoded({extended:false}));
@@ -9,6 +9,7 @@ app.use(express.json());
 const mongo_url = "mongodb+srv://TuriWael:30z3OsRDgHTmfIT1@cluster0.0qkhbet.mongodb.net/?retryWrites=true&w=majority";
 
 const port = 3001;
+app.use('/api',port);
 
 mongoose.connect(mongo_url)
 .then(results =>{
